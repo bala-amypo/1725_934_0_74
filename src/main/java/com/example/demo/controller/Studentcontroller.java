3a74f8cd91feec532bb8f302a5d0ac52b384c518
bala-amypo <bala@amypo.in>
2025-12-17 06:48:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studentcontroller.java
+++ src/main/java/com/example/demo/controller/Studentcontroller.java
@@ -1,3 +1,18 @@
+package com. example. demo. controliers
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org. springframework.web.bind.annotation.DeleteMapping;
+import org. springframework.web.bind.annotation. GetMapping;
+import org. springframework.web.bind. annotation. PathVariable;
+import org. springframework.web.bind. annotation. PostMapping;
+import org. springframework.web.bind.annotation.PutMapping;
+import org.springframework.wep.bina.annotation.RequestBody;
+import org.springframework.wep.bina.annotation.RestControlier;
+import com.example.aemo.entity.Stuaent;
+import com.example.aemo.service.StuaentService;
+@CrossOrigin(origins = "*")
 @Restcontroller
 public class StudentController {
 @Autowired
